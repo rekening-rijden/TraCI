f2010861a678db67d5680d4a846e4469dd1cc388
Luc Janssen <lphjanss@gmail.com>
2022-10-11 15:26:00 +0200

    Added switch for lane speed to determine road type

--- TraCI Interface.py
+++ TraCI Interface.py
@@ -19,7 +19,23 @@ responseTimes = []
 #Converts simulation coordinates to geo coordinates
 def send_request(x, y, vId):
     geolocation = traci.simulation.convertGeo(x, y)
-    print(vId + ': ' + str(geolocation[0]) + ', ' + str(geolocation[1]))
+    #Retreive tha current lane the vehicle is on
+    laneId = traci.vehicle.getLaneID(vId)
+    #Get the lanespeed for the current lane and convert from m/s to mp/h
+    laneSpeed = round(traci.lane.getMaxSpeed(laneId) * 2.23694, 0)
+    roadType = ""
+    print(laneSpeed)
+    #Match lane speed to road type
+    match laneSpeed:
+        case 20:
+            roadType = "Built-up area"
+        case 30:
+            roadType = "Built-up area"
+        case 60:
+            roadType = "Single carriageway"
+        case 70:
+            roadType = "Dual carriageway / Motorway"
+    print(vId + ': ' + str(geolocation[0]) + ', ' + str(geolocation[1]) + ', ' + roadType)
 
 #Multithreading Runner
 def runner(_vehicleList, locationList):
